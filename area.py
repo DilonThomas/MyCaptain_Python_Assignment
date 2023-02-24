cf0c11334f256c7782c97361b3c52df229bbdf90
DilonThomas <126073205+DilonThomas@users.noreply.github.com>
2023-02-24 22:17:46 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,10 @@
+# Python program to print the area of a circle
+
+# Input the radius
+x = float(input("Enter the radius of the circle: "))
+
+# Calculating the radius
+area = 3.14*x*x
+
+#Printing the desired result
+print("The area of the circle with radius {x} is: ", area)
